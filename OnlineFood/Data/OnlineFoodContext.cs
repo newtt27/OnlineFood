3f791f605145cc60063d155091ede7edb11963ce
Duy Le <l.a.d11052003@gmail.com>
2024-12-14 21:27:32 +0700

    Add hình ảnh cho food thành công

--- OnlineFood/Data/OnlineFoodContext.cs
+++ OnlineFood/Data/OnlineFoodContext.cs
@@ -63,7 +63,7 @@ public partial class OnlineFoodContext : DbContext
     public virtual DbSet<Supply> Supplies { get; set; }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        => optionsBuilder.UseSqlServer("Server=localhost;Database=OnlineFood;User Id=sa;Password=123456789;TrustServerCertificate=True; ");
+        => optionsBuilder.UseSqlServer("Server=ACERNITRO\\SQLEXPRESS;Database=ONLINE1;User Id=sa;Password=123;TrustServerCertificate=True; ");
 
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
